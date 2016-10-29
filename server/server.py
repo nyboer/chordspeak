896fe6df366edf85ed4c8626b8384f198b02d894
Kensaku Komtasu <kensaku.komatsu@gmail.com>
2016-10-29 14:13:02 -0700

    implemented parsing notes

--- server/server.py
+++ server/server.py
@@ -2,9 +2,70 @@ from flask import Flask, request, send_from_directory
 from flask_cors import CORS, cross_origin
 from string import Template
 
+import json
+import re
+
 '''app = Flask(__name__, static_url_path='/public')'''
 app = Flask(__name__, static_url_path='')
 CORS(app)
+pattern = r"^[abcdefgABCDEFG]$"
+
+def parser(str):
+    chords = str.split(" ")
+    idx = 0
+    ret = {"notes": []}
+
+    for chord in chords:
+        note = ""
+        if(chord == "major"
+                or chord == "minor"
+                or chord == "diminished"
+                or chord == "dominant"
+                or chord == "half"
+                ):
+            # parsing notes
+            _l = chords[idx - 1]
+            if(_l == "sharp" or _l == "flat"):
+                note = chords[idx - 2].lower() + " " + _l.lower()
+            elif re.match(pattern, _l):
+                note = _l.lower()
+
+            # parsing major, minor, etc.
+            params = []
+            if(len(chords) > (idx + 1) and chords[idx + 1] == "seventh"):
+                chord += " seventh"
+            elif chord == "half" and chords[idx + 1] == "diminished" and chords[idx + 2] == "seventh":
+                chord = "half diminished seventh"
+
+            params.append(chord)
+
+            # parsing pitch, augumented
+            seq = [1,2,3,4]
+            for num in seq:
+                if(len(chords) > (idx + num)):
+                    if(chords[idx + num] == "pitch" or chords[idx + num] == "augmented"):
+                        params.append(chords[idx + num])
+
+            if(note != ""):
+                ret["notes"].append({
+                    "note": note,
+                    "params": params
+                })
+
+        if(chord == "transpose"):
+            ret["transpose"] = chords[idx + 1]
+        if(chord == "octave"):
+            ret["octave"] = chords[idx + 1]
+        if(chord == "arpeggiate"):
+            ret["arpeggiate"] = True
+        if(chord == "play"):
+            ret["play"] = True
+
+        idx += 1
+
+
+
+    return ret
 
 @app.route("/")
 def index():
@@ -13,18 +74,22 @@ def index():
 
 @app.route("/chord/")
 def chord():
-    chord = request.args.get('transcript')
+    chords = request.args.get('transcript')
     confidence = request.args.get('confidence')
 
     '''do midi stuff with chord request'''
-    t = Template("chord request='${chord}' (${confidence})")
+    t = Template("request='${chords}' (${confidence})")
 
-    str = t.substitute({"chord": chord, "confidence": confidence})
+    str = t.substitute({"chords": chords, "confidence": confidence})
     print("========================================")
     print(str)
     print("========================================")
 
-    return "ok"
+    resp = parser(chords)
+
+    print json.dumps(resp, indent=4, sort_keys=True)
+
+    return json.dumps(resp)
 
 if __name__ == "__main__":
     app.run()
---
+++ server/testscript.py
@@ -0,0 +1,28 @@
+import httplib2
+import urllib
+
+endpoint="http://localhost:5000/chord/"
+testscript="testscript.txt"
+
+'''
+dummy= {
+ "transcript": "D minor play transpose 5 augmented",
+ "confidence": 0.9
+}
+'''
+
+
+file = open(testscript, 'r')
+
+scripts = file.read().split("\n")
+h = httplib2.Http(".cache")
+
+for script in scripts:
+    if len(script) != 0:
+        dict = { "transcript": script, "confidence": 0.9 }
+        '''condidence is just a dummy num'''
+        data = urllib.urlencode(dict)
+        print script
+        resp, content = h.request(endpoint+"?"+data, "GET")
+        print(content)
+
